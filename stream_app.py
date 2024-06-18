85c38fb78af826274eae4c6959f210f6159d053b
Patharanan <96046535+Patharanan@users.noreply.github.com>
2024-06-18 08:34:23 +0700

    Update stream_app.py

--- stream_app.py
+++ stream_app.py
@@ -1,3 +1,33 @@
 import streamlit as st
+from PIL import Image
+import random
 
-st.write("Hello world!")
+# รูปภาพที่ใช้ในเกม
+images = [
+    'image1.jpg', 'image2.jpg', 'image3.jpg'
+]
+
+# เริ่มต้นเกม
+def play_matching_game():
+    st.title('Trip Korat EP2')
+    st.write('Select day for ')
+
+
+    
+    # แบ่งหน้าจอเป็นคอลัมน์ 4 คอลัมน์
+    col1, col2, col3, col4 = st.columns(4)
+
+    # แสดงรูปภาพในแต่ละคอลัมน์
+    if col1.button('Day 1'):
+        st.image(Image.open( 'image1.jpg'), use_column_width=True)
+    if col2.button('Day 2'):
+        st.image(Image.open( 'image2.jpg'), use_column_width=True)
+    if col3.button('Day 3'):
+        st.image(Image.open( 'image3.jpg'), use_column_width=True)
+  
+
+    # if st.button('Reset'):
+    #     play_matching_game()
+
+# เรียกฟังก์ชันเริ่มต้นเกม
+play_matching_game()
