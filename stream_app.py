f88a49a15300f5ff6e4fbad770f4b1c5471f1274
Patharanan <96046535+Patharanan@users.noreply.github.com>
2024-07-26 08:31:23 +0700

    Update stream_app.py

--- stream_app.py
+++ stream_app.py
@@ -107,7 +107,6 @@ st.markdown("""
                     <li>Use the slider menu to configure the `timediff(ms)` sets a value that does not exceed the calculation to eliminate errors. The resulting graph is displayed on the right side of the screen.</li>
             </ol>
         </p>
-      
         <h3>Data Page</h3>
         <p class="indent">
             It is a page linked to the Normal page. It displays information about 
